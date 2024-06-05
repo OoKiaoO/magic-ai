d0e422b0a6fd9160907e8677b6c42a9e06c029a7
Chiara Campalani <campalanichiara@gmail.com>
2024-06-05 15:29:56 +0900

    applied new tailwind & css global configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
 
 const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"], weight: ['400', '500', '600', '700'], variable: '--font-ibm-plex' });
 
